4d91fd24d212a1e110b816ca59ddddc6d8e5f81f
SND\Heisenbug13_cp <SND\Heisenbug13_cp@937343c3-697a-4e39-b5c6-83b50a1b7d63>
2012-07-12 13:58:40 +0000

    git-svn-id: https://gameaudiosystem.svn.codeplex.com/svn@17612 937343c3-697a-4e39-b5c6-83b50a1b7d63

--- GAS/GAS/GameAudioWrapper.cpp
+++ GAS/GAS/GameAudioWrapper.cpp
@@ -1,12 +1,23 @@
 #include "GameAudioWrapper.h"
 #include <iostream>
+#include <ogg\ogg.h>
+
+#include <vorbis\vorbisfile.h>
+#include <vector>
+#include <AL\alut.h>
 
 using namespace GAS;
+
+const ALfloat GameAudioWrapper::DEFAULT_LISTENER_POS[3] = {0.0f,0.0f,0.0f};
+const ALfloat GameAudioWrapper::DEFAULT_LISTENER_VEL[3] = {0.0f,0.0f,0.0f};
+const ALfloat GameAudioWrapper::DEFAULT_LISTENER_ORIENTATION[6] = {0.0f,0.0f,-1.0f, 0.0f, 1.0f, 0.0f};
+
 
 GameAudioWrapper::GameAudioWrapper(void)
 {
 	Init();
 //	InitEAX();
+	InitListener();
 }
 
 
@@ -16,7 +27,9 @@ GameAudioWrapper::~GameAudioWrapper(void)
 
 bool GameAudioWrapper::Init()
 {
-	mSoundDevice = alcOpenDevice( nullptr );
+	
+	
+	mSoundDevice = alcOpenDevice( NULL );
 	if ( !mSoundDevice )
 	{
 		std::cout << "GameAudioWrapper: Sound Device creation FAILED" <<std::endl;
@@ -37,14 +50,16 @@ bool GameAudioWrapper::Init()
 		return false;
 	}
 
-
+//	alutInit(NULL,NULL);
 	// Create the Audio Buffers
-   alGenBuffers( MAX_AUDIO_BUFFERS, mAudioBuffers );
-   if (CheckErrors("alGenBuffers"))
+	alGenBuffers( MAX_AUDIO_BUFFERS, mAudioBuffers );
+	//alGenBuffers( 1, mAudioBuffers );
+   if (CheckErrors("algenbuffers"))
         return false;
 
-   // Generate Sources
+   // generate sources
    alGenSources( MAX_AUDIO_SOURCES, mAudioSources );
+   //alGenSources( 1, mAudioSources );
    if (CheckErrors( "alGenSources"))
         return false;
 
@@ -67,6 +82,18 @@ bool GameAudioWrapper::InitEAX()
 	ALchar szAC3[] = "EAX-AC3";
 	mActiveEAX_AC3 = alIsExtensionPresent(szAC3);
 
+	return true;
+}
+bool GameAudioWrapper::InitListener()
+{
+
+	alListenerfv( AL_POSITION, DEFAULT_LISTENER_POS );
+	alListenerfv( AL_VELOCITY, DEFAULT_LISTENER_VEL );
+	
+	alListenerfv( AL_ORIENTATION, DEFAULT_LISTENER_ORIENTATION );
+	alDopplerFactor( 1.0 ); // 1.2 = exaggerate the pitch shift by 20%
+	alDopplerVelocity( 343.0f ); // m/s this may need to be scaled at some point
+ 
 	return true;
 }
 bool GameAudioWrapper::CheckErrors(const std::string& iSource) const
@@ -101,88 +128,195 @@ bool GameAudioWrapper::CheckErrors(const std::string& iSource) const
  
    return true;
  }
+bool GameAudioWrapper::IsLoadedSound(std::string& iSoundName)const
+{
+	BufferNameMap::const_iterator it = mBufferNames.find(iSoundName);
+	if (it != mBufferNames.end())
+		return true;
+	return false;
+}
+unsigned int GameAudioWrapper::CreateSource(std::string& iFileName, std::string& iFormat)
+{
+	
+	
+	std::string name = iFileName + "." + iFormat;
+	if (!IsLoadedSound(name))
+	{
+		LoadSound(iFileName,iFormat);
+	}
 
+	alGetError();
+	unsigned int bufferId = mBufferNames[name];
+	int sourceId = GetFreeSource();
+
+	if (sourceId == -1)
+		throw;
+
+	alSourcei( mAudioSources[sourceId], AL_BUFFER, mAudioBuffers[bufferId] );
+
+	mUsedSources.set(sourceId);
+
+	if (CheckErrors("GameAudioWrapper::CreateSource"))
+		throw;
+	return  mAudioSources[sourceId];
+}
 void GameAudioWrapper::LoadSound(std::string& iFileName, std::string& iFormat)
 {
 	if (!IsFormatSupported(iFormat))
 	{
 		std::cout << "GameAudioWrapper: ERROR Audio Format Not Supported " << iFormat << std::endl;
-		return;
+		throw;
 	}
 
+	int bufferIndex = GetFreeBuffer();
+	if (bufferIndex == -1)
+	{
+	    std::cout << "GameAudioWrapper: ERROR NO FREE BUFFER " <<std::endl;
+		throw;
+	}
 
+	if (iFormat == "ogg")
+		LoadOgg(iFileName + "." + iFormat, mAudioBuffers[bufferIndex]);
+	if ((iFormat == "wav"))
+		LoadWav(iFileName + "." + iFormat, mAudioBuffers[bufferIndex]);
+	mUsedBuffers.set(bufferIndex);
+ 
+	mBufferNames[iFileName + "." + iFormat] = bufferIndex;
 }
 
 bool GameAudioWrapper::IsFormatSupported(std::string& iFormat)const
 {
-	if (iFormat == "ogg")
+	if (iFormat == "ogg" || iFormat == "wav")
 		return true;
 	return false;
 }
-
-void GameAudioWrapper::LoadOgg(const std::string& iFileName)
+void GameAudioWrapper::LoadWav(const std::string& iFileName, unsigned int iBufferId)
 {
-  
-    OggVorbis_File oggfile;
+	ALuint buffer, source;
+	buffer = alutCreateBufferFromFile(iFileName.c_str());
+	alGenSources (1, &source);
+	alSourcei (source, AL_BUFFER, buffer);
+	alSourcei(source, AL_LOOPING, AL_TRUE);
+	alSourcePlay (source);
+	alutSleep (30);
+	alutSleep (30);
+
+}
+
+void GameAudioWrapper::LoadOgg(const std::string& iFileName, unsigned int iBufferId)
+{
  
-    if(ov_fopen(const_cast<char*>(filename.c_str()), &oggfile))
-    {
-        printf("SoundManager::loadOGG() : ov_fopen failed.\n");
-        return false;
-    }
- 
-    vorbis_info* info = ov_info(&oggfile, -1);
- 
+#define BUFFER_SIZE 4096
+	int endian = 0;                         // 0 for Little-Endian, 1 for Big-Endian
+    int bitStream;
+    char array[BUFFER_SIZE];                // Local fixed size array
+    FILE *f;
+
+    // Open for binary reading
+	f = fopen(iFileName.c_str(), "rb");
+
+    if (f == NULL)
+		throw;
+    
+    OggVorbis_File oggFile;
+
+    // Try opening the given file
+    if (ov_open(f, &oggFile, NULL, 0) != 0)
+		throw;
+
+	vorbis_info* info = ov_info(&oggFile, -1);;
     ALenum format;
     switch(info->channels)
     {
         case 1:
-            format = AL_FORMAT_MONO16; break;
+            format = AL_FORMAT_MONO16;
+			break;
         case 2:
-            format = AL_FORMAT_STEREO16; break;
+            format = AL_FORMAT_STEREO16;
+			break;
         case 4:
-            format = alGetEnumValue("AL_FORMAT_QUAD16"); break;
+            format = alGetEnumValue("AL_FORMAT_QUAD16"); 
+			break;
         case 6:
-            format = alGetEnumValue("AL_FORMAT_51CHN16"); break;
+            format = alGetEnumValue("AL_FORMAT_51CHN16"); 
+			break;
         case 7:
-            format = alGetEnumValue("AL_FORMAT_61CHN16"); break;
+            format = alGetEnumValue("AL_FORMAT_61CHN16"); 
+			break;
         case 8:
-            format = alGetEnumValue("AL_FORMAT_71CHN16"); break;
+            format = alGetEnumValue("AL_FORMAT_71CHN16"); 
+			break;
         default:
-            format = 0; break;
+            format = 0; 
+			break;
     }
- 
-    std::vector<int16> samples;
-    char tmpbuf[4096];
-    int section = 0;
-    bool firstrun = true;
-    while(1)
+	std::vector<char> samples;
+	long bytes;
+	// Keep reading until all is read
+    do
     {
-        int result = ov_read(&oggfile, tmpbuf, 4096, 0, 2, 1, &section);
-        if(result > 0)
-        {
-            firstrun = false;
-            samples.insert(samples.end(), tmpbuf, tmpbuf + (result));
-        }
-        else
-        {
-            if(result < 0)
-            {
-                printf("SoundManager::loadOGG() : Loading ogg sound data failed!");
-                ov_clear(&oggfile);
-                return false;
-            }
-            else
-            {
-                if(firstrun)
-                    return false;
-                break;
-            }
-        }
-    }
+		// Read up to a buffer's worth of decoded sound data
+		bytes = ov_read(&oggFile, array, BUFFER_SIZE, endian, 2, 1, &bitStream);
+
+		if (bytes < 0)
+			{
+			ov_clear(&oggFile);
+		//     cerr << "Error decoding " << fileName << "..." << endl;
+			exit(-1);
+			}
+		// end if
+
+		// Append to end of buffer
+		samples.insert(samples.end(), array, array + bytes);
+	}while (bytes > 0);
+
+ 	alBufferData(iBufferId, format, &samples[0], static_cast<ALsizei>(samples.size()), info->rate);
+
+ 	ov_clear(&oggFile);
+	if ( CheckErrors( "LoadOgg") )
+		throw;
+}
+
+int GameAudioWrapper::GetFreeBuffer()const
+{
+	int size = mUsedBuffers.size();
+	for (int index = 0 ; index < size ; ++index)
+	{
+		if (!mUsedBuffers.test(index))
+			return index;
+	}
+	return -1;
+}
+
+int GameAudioWrapper::GetFreeSource()const
+{
+	int size = mUsedSources.size();
+
+	for (int index = 0 ; index < size ; ++index)
+	{
+		if (!mUsedSources.test(index))
+			return index;
+	}
+	return -1;
+}
+
+bool GameAudioWrapper::Play(unsigned int iSoundId)const
+{
+	int sourceAudioState = 0;
  
-    alBufferData(pDestAudioBuffer, format, &samples[0], ov_pcm_total(&oggfile, -1), info->rate);
+	alGetError();
  
-    return true;
- }
+ 
+	ALint state ;
+	alSourcePlay(iSoundId);
+	// This is a busy wait loop but should be good enough for example purpose
+	do {
+		// Query the state of the souce
+		alGetSourcei(iSoundId, AL_SOURCE_STATE, &state);
+	} while (state != AL_STOPPED);
+	
+	if ( CheckErrors( "playAudio::alSourcePlay: ") )
+		return false;
+ 
+	return true;
 }
--- GAS/GAS/GameAudioWrapper.h
+++ GAS/GAS/GameAudioWrapper.h
@@ -5,11 +5,12 @@
 #include <eax.h>
 #include <al.h>
 #include <alc.h>
-#include <ogg\ogg.h>
 
 #include <string>
 #include <bitset>
+#include <unordered_map>
 
+#include "AudioObjectsPool.h"
 
 namespace GAS
 {
@@ -21,22 +22,33 @@ public:
 	~GameAudioWrapper(void);
 
 	void LoadSound(std::string& iFileName, std::string& iFormat);
+	unsigned int CreateSource(std::string& iFileName, std::string& iFormat);
 	bool IsFormatSupported(std::string& iFormat) const;
+	bool IsLoadedSound(std::string& iSoundName)const;
+	bool Play(unsigned int iSoundId)const;
 private:
 	bool Init();
 	bool InitEAX();
 	bool InitListener();
 
+	
+
 	bool CheckErrors(const std::string& iSource) const;
 
+	int GetFreeBuffer()const;
+	int GetFreeSource()const;
 
-	void LoadOgg(const std::string& iFileName);
+	void LoadOgg(const std::string& iFileName, unsigned int iBufferId);
+	void LoadWav(const std::string& iFileName, unsigned int iBufferId);
 	//----------------- MEMBER FIELDS ---------------------------
 	ALCdevice* mSoundDevice;
 	ALCcontext* mSoundContext;
 
 	static const int MAX_AUDIO_BUFFERS = 10;
 	static const int MAX_AUDIO_SOURCES = 10;
+	static const ALfloat DEFAULT_LISTENER_POS[3];
+	static const ALfloat DEFAULT_LISTENER_VEL[3];
+	static const ALfloat DEFAULT_LISTENER_ORIENTATION[6];// = {0.0f, 0.0f,0.0f};
 
 	unsigned int mAudioBuffers[ MAX_AUDIO_BUFFERS ];
 	std::bitset<MAX_AUDIO_BUFFERS> mUsedBuffers;
@@ -44,6 +56,8 @@ private:
 	unsigned int mAudioSources[ MAX_AUDIO_SOURCES ];
 	std::bitset<MAX_AUDIO_BUFFERS> mUsedSources;
 
+	typedef std::unordered_map<std::string, unsigned int> BufferNameMap;
+	BufferNameMap mBufferNames;
 
 	//------ EAX extensions --------------
 	bool mActiveEAX;
